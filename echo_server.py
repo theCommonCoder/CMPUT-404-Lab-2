02e01f68235d53333b675210375d5f49b319b7c0
Mason <mdbly@ualberta.ca>
2023-09-19 12:58:43 -0600

    Update to multithreading

--- echo_server.py
+++ echo_server.py
@@ -1,26 +1,39 @@
 import socket
 import time
+from threading import Thread
 
 HOST = "127.0.0.1"
 PORT = 8080
 buffer_size = 2048
 
 
-def main():
-    with socket.socket() as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((HOST, PORT))
-        s.listen(2)
-        while True:
-            conn, addr = s.accept()
-            print("Connected by", addr)
+def handle_connection(conn, addr):
+    with conn:
+        print(f"Connected by {addr}")
 
-            # receive data, wait a bit, then send it back
-            full_data = conn.recv(buffer_size)
-            time.sleep(0.5)
-            print(conn.recv(buffer_size))
-            conn.sendall(full_data)
-            conn.close()
+        all_data = b""
+        while True:
+            data = conn.recv(buffer_size)
+            if not data:
+                break
+            all_data += data
+        conn.sendall(all_data)
+
+
+def start_threaded_server():
+    with socket.socket() as server_socket:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(3)
+
+        while True:
+            conn, addr = server_socket.accept()
+            thread = Thread(target=handle_connection, args=(conn, addr))
+            thread.run()
+
+
+def main():
+    start_threaded_server()
 
 
 if __name__ == "__main__":
