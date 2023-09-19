527a0006e0f73fe76a7e5839490237462324d287
Mason <mdbly@ualberta.ca>
2023-09-19 12:39:10 -0600

    Update lab code

--- echo_server.py
+++ echo_server.py
@@ -8,10 +8,9 @@ buffer_size = 2048
 
 def main():
     with socket.socket() as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen(2)
-        conn, addr = s.accept()
-        print("Connected by", addr)
         while True:
             conn, addr = s.accept()
             print("Connected by", addr)
@@ -19,6 +18,7 @@ def main():
             # receive data, wait a bit, then send it back
             full_data = conn.recv(buffer_size)
             time.sleep(0.5)
+            print(conn.recv(buffer_size))
             conn.sendall(full_data)
             conn.close()
 
